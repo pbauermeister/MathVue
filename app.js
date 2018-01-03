4cb2fa94ed68bfeb5761131e7b5bdf4e873b8bec
Pascal Bauermeister <pascal@privately.eu>
2018-01-03 19:57:56 +0100

    Separated processing glue from Vue app. Added Resume/Pause buttons.

--- app.js
+++ app.js
@@ -1,79 +1,30 @@
-// default formula
-var defaultFormula = `
-WIDTH = 300;
-RATIO = 1;
-X_MIN = -20; X_MAX = 20;
-Y_MIN = -20; Y_MAX = 20;
-MOUSE_MOVE = true;
+/*
+ * Vue.js app.
+ */
 
-color hsb(x, y) {
-  float d = dist(u, v, mouseX, mouseY) / WIDTH * 10;
-  float shift = mouseX / WIDTH;
-
-  float bright = (1/d) * 255;
-  float hue = shift * 255;
-
-  return color(hue, 255, bright);
-}
-`.trim();
-
-// Processing stuff
-
-var processingInstance = null;
-
-function loadSketch(formula) {
-  // assemble a full PDE out of the user formula and mathvision template
-  var patt = new RegExp("Start of user formula @@@(.|\n)*?"+
-                        "End of user formula @@@", "gm");
-  // TODO: clean formula from code injection and remove /*...*/ comments
-  var text = "Start of user formula @@@\n"+ formula +
-      "\n// End "+"of user formula @@@";
-
-  // compile it by Processing.js
-  var code = mathVisionTemplate.replace(patt, text); // TODO: use function to avoid \1
-  var sketch = Processing.compile(code);
-  var canvas = document.getElementById("mathvisionCanvas");
-
-  // (re)load processing on canvas
-  if(processingInstance) {
-    processingInstance.exit();  // cleanup
-  }
-  try {
-    processingInstance = new Processing(canvas, sketch);
-    processingInstance.loop();
-  }
-  catch (e){
-    alert(e);
-  }
-}
-
-// Bindings to the web page, through Vue.js
 var app = new Vue({
   el: '#app',
 
   data: {
     message: 'Hello MathVue! With Bootstrap 4.0.',
-    formula: defaultFormula
+    formula: defaultFormula,
+    running: false,
+    started: false
   },
 
   methods: {
     run: function (event) {
+      this.running = true;
+      this.started = true;
       loadSketch(this.formula);
+    },
+    pause: function (event) {
+      this.running = false;
+      switchSketchState(false);
+    },
+    resume: function (event) {
+      this.running = true;
+      switchSketchState(true);
     }
   }
-})
-
-// Loading of the MathVision PDE template
-var mathVisionTemplate = "";
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    if (xhr.status == 200) {
-      mathVisionTemplate = xhr.responseText;
-    } else {
-      alert("Could not load the MathVision PDE template.");
-    }
-  }
-};
-xhr.open("GET", "mathvision-template.pde");
-xhr.send();
+});
---
+++ pjs_glue.js
@@ -0,0 +1,79 @@
+/*
+ * Glue to Processing.js
+ */
+
+// default formula
+var defaultFormula = `
+WIDTH = 300;
+RATIO = 1;
+X_MIN = -20; X_MAX = 20;
+Y_MIN = -20; Y_MAX = 20;
+MOUSE_MOVE = true;
+
+color hsb(x, y) {
+  float d = dist(u, v, mouseX, mouseY) / WIDTH * 10;
+  float shift = mouseX / WIDTH;
+
+  float bright = (1/d) * 255;
+  float hue = shift * 255;
+
+  return color(hue, 255, bright);
+}
+`.trim();
+
+// Processing stuff
+
+var processingInstance = null;
+
+function loadSketch(formula) {
+  // assemble a full PDE out of the user formula and mathvision template
+  var patt = new RegExp("Start of user formula @@@(.|\n)*?"+
+                        "End of user formula @@@", "gm");
+  // TODO: clean formula from code injection and remove /*...*/ comments
+  var text = "Start of user formula @@@\n"+ formula +
+      "\n// End "+"of user formula @@@";
+
+  // compile it by Processing.js
+  var code = mathVisionTemplate.replace(patt, text); // TODO: use function to avoid \1
+  var sketch = Processing.compile(code);
+  var canvas = document.getElementById("mathvisionCanvas");
+
+  // (re)load processing on canvas
+  if(processingInstance) {
+    processingInstance.exit();  // cleanup
+  }
+  try {
+    processingInstance = new Processing(canvas, sketch);
+    processingInstance.loop();
+  }
+  catch (e){
+    alert(e);
+  }
+}
+
+function switchSketchState(on) {
+  if (!processingInstance) {
+    processingInstance = Processing.getInstanceById('sketch');
+  }
+  
+  if (on) {
+    processingInstance.loop();  // call Processing loop() function
+  } else {
+    processingInstance.noLoop(); // stop animation, call noLoop()
+  }
+}
+
+// Loading of the MathVision PDE template
+var mathVisionTemplate = "";
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4) {
+    if (xhr.status == 200) {
+      mathVisionTemplate = xhr.responseText;
+    } else {
+      alert("Could not load the MathVision PDE template.");
+    }
+  }
+};
+xhr.open("GET", "mathvision-template.pde");
+xhr.send();
