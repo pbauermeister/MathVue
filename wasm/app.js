cae665a4156fece629194226af9f029bd0c28532
Pascal Bauermeister <pascal.bauermeister@depsys.com>
2020-11-11 07:57:59 +0100

    Belongs to last commit.

--- wasm/app.js
+++ wasm/app.js
@@ -1,37 +1,3 @@
-Vue.component('Editor', {
-  template: '<div :id="editorId" style="width: 100%; height: 100%;"></div>',
-  props: ['editorId', 'content', 'lang', 'theme'],
-  data () {
-    return {
-      editor: Object,
-      beforeContent: ''
-    }
-  },
-  watch: {
-    'content' (value) {
-      if (this.beforeContent !== value) {
-        this.editor.setValue(value, 1)
-      }
-    }
-  },
-  mounted () {
-    const lang = this.lang || 'text'
-    const theme = this.theme || 'github'
-
-    this.editor = window.ace.edit(this.editorId)
-    this.editor.setValue(this.content, 1)
-
-    this.editor.getSession().setMode(`ace/mode/${lang}`)
-    this.editor.setTheme(`ace/theme/${theme}`)
-
-    this.editor.on('change', () => {
-      this.beforeContent = this.editor.getValue()
-      this.$emit('change-content', this.editor.getValue())
-    })
-  }
-})
-
-
 /*
  * Vue.js app.
  */
