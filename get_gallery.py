8762efef343d31a16aa58ffb7d196716de5bff4f
Pascal Bauermeister <pascal.bauermeister@depsys.com>
2022-11-25 22:03:15 +0100

    get_gallery.py: do it concurrently ==> 39s -> 12s

--- get_gallery.py
+++ get_gallery.py
@@ -6,6 +6,8 @@ import json
 import os
 import requests
 import sys
+from threading import Thread, Lock
+from datetime import datetime
 
 token = os.environ.get('TOKEN')
 
@@ -21,6 +23,7 @@ headers = {
 }
 
 FileInfo = namedtuple('FileInfo', ('name', 'formula_url', 'thumb_url'))
+
 
 def get_all_files(items=None, cursor=None):
     items = items or []
@@ -52,6 +55,7 @@ def get_all_files(items=None, cursor=None):
 
     return items
 
+
 def get_relevant_files_infos(suffix, items, startswith=None):
     nbi = {item['id']:item['name'] for item in items}
     if startswith:
@@ -61,12 +65,17 @@ def get_relevant_files_infos(suffix, items, startswith=None):
     id_by_thumb = {name:id for id, name in nbi.items() if name.endswith('.png')}
 
     res = []
-    for name, code_id in sorted(id_by_code.items()):
+    lock = Lock()
+
+    def f(name, code_id):
         thumb_id = id_by_thumb.get(name+'.png')
-        #print("***", name, thumb_id)
+        start = datetime.now()
+        #print('>>>', name, 'id', thumb_id, file=sys.stderr)
         response = requests.post('https://api.dropboxapi.com/2/files/get_temporary_link',
                                  headers=headers,
                                  json=dict(path=code_id))
+        duration = datetime.now() - start
+        #print('<<<1 code url in', duration, name, file=sys.stderr)
         if response.ok:
             code_url = response.json()['link']
             thumb_url = None
@@ -74,10 +83,21 @@ def get_relevant_files_infos(suffix, items, startswith=None):
                 response = requests.post('https://api.dropboxapi.com/2/files/get_temporary_link',
                                          headers=headers,
                                          json=dict(path=thumb_id))
+                duration = datetime.now() - start
+                #print('<<<2 thumb url after', duration, name, file=sys.stderr)
                 if response.ok:
                     thumb_url = response.json()['link']
-            res.append(FileInfo(name, code_url, thumb_url)._asdict())
+            with lock:
+                res.append(FileInfo(name, code_url, thumb_url)._asdict())
+
+    # create and run threads
+    threads = [Thread(target=f, args=(name, code_id))
+               for name, code_id in sorted(id_by_code.items())]
+    for thread in threads: thread.start()
+    for thread in threads: thread.join()
+
     return res
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Get DropBox MathVue public files.')
@@ -91,6 +111,7 @@ def parse_args():
     group.add_argument('--startswith',
                        help='list links of code files, for matching files')
     return parser.parse_args()
+
 
 args = parse_args()
 items = get_all_files()
